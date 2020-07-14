c2ad01ce846ba47c11677defb54f121421f55449
Yogendra Kumar <60997584+yogiyadav08@users.noreply.github.com>
2020-07-14 15:40:27 +0530

    Update multiples_of_three_and_five.js

--- JavaScript/multiples_of_three_and_five.js
+++ JavaScript/multiples_of_three_and_five.js
@@ -1,11 +1,11 @@
 function multiplesOf3and5(number) {
   let arr = [];
-  let num = number
-  while (num > 0) {
+  let num = 3
+  while (num < number) {
     if (num % 3 == 0 || num % 5 == 0) {
        arr.push(num)
     }
-    num--;
+    num++;
   }
   let sum = arr.reduce((a, b) => (a + b));
 
